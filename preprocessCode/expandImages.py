9a79e3e636c6fed2753c03515caaa51401027bb0
Eero Salli <83005928+esalli@users.noreply.github.com>
2021-08-25 18:08:21 +0300

    enh: remove extra code

--- preprocessCode/expandImages.py
+++ preprocessCode/expandImages.py
@@ -21,32 +21,18 @@ for sph in c.SPHEROIDS:
     h=int(expandFactor[i])
     #expansion factor
     h=3
+    
     hstr=str(h)
     #ground truth
     GT=sitk.ReadImage(c.PREPROCESSED_GT_DATADIR+sph+'_GT.nrrd')
     GTExpanded=sitk.Expand(GT,expandFactors=[1,1,h],interpolator=sitk.sitkNearestNeighbor)
-    sitk.WriteImage(GTExpanded,c.PREPROCESSED_GT_DATADIR+sph+'_GT_expanded_nearest_3.nrrd',useCompression=True)
     GTExpandedSparse=GTExpanded*0
     
-    #morpho_interpolator= 
     for j in range(1, GTExpanded.GetSize()[2]+1, h):
         GTExpandedSparse[:,:,j]=GTExpanded[:,:,j]
         
-    # use nearest neighbor interpolation for the slices near the bottom and top of the image stack because itk.morphological_contour_interpolator does not seem to handle them properly
-    #GTExpandedSparse[:,:,0]=GTExpanded[:,:,0]
-    #GTExpandedSparse[:,:,1]=GTExpanded[:,:,1]
-    #GTExpandedSparse[:,:,2]=GTExpanded[:,:,2]
-    #GTExpandedSparse[:,:,3]=GTExpanded[:,:,3]
-    #GTExpandedSparse[:,:,4]=GTExpanded[:,:,4]
-    
-    #GTExpandedSparse[:,:,GTExpanded.GetSize()[2]-1]=GTExpanded[:,:,GTExpanded.GetSize()[2]-1]
-    #GTExpandedSparse[:,:,GTExpanded.GetSize()[2]-2]=GTExpanded[:,:,GTExpanded.GetSize()[2]-2]
-    #GTExpandedSparse[:,:,GTExpanded.GetSize()[2]-3]=GTExpanded[:,:,GTExpanded.GetSize()[2]-3]
-    #GTExpandedSparse[:,:,GTExpanded.GetSize()[2]-4]=GTExpanded[:,:,GTExpanded.GetSize()[2]-4]
-    #GTExpandedSparse[:,:,GTExpanded.GetSize()[2]-5]=GTExpanded[:,:,GTExpanded.GetSize()[2]-5]
     
     
-    sitk.WriteImage(GTExpandedSparse,c.PREPROCESSED_GT_DATADIR+sph+'_GT_expanded_sparse_3.nrrd',useCompression=True)
         
     itk_image = itk.GetImageFromArray(sitk.GetArrayFromImage(GTExpandedSparse), is_vector = GTExpandedSparse.GetNumberOfComponentsPerPixel()>1)
     itk_image.SetOrigin(GTExpandedSparse.GetOrigin())
@@ -56,9 +42,6 @@ for sph in c.SPHEROIDS:
     
     filled = itk.morphological_contour_interpolator(itk_image,axis=2,UseDistanceTransform=False)
     filled.Update()
-    #filled.SetAxis(2)
-    
-#    itk.imwrite(filled,c.PREPROCESSED_GT_DATADIR+sph+'_GT_expanded.nrrd' , compression=True)
     
     
     # Back to a simpleitk image from the itk image
@@ -84,8 +67,6 @@ for sph in c.SPHEROIDS:
     closedImage_np=sitk.GetArrayFromImage(closedImage)
     
     differenceImage_np=(filled_np_binary-closedImage_np)!=0
-    #in order to change value we require that 1) differenceImage > 0 
-    #maskImage_np=differenceImage_np*(GTExpanded_np>0)
     
     #where differenceImage is true we use NN, otherwise countour interplation
     filled_np=np.where(differenceImage_np,GTExpanded_np,filled_np)
@@ -97,11 +78,6 @@ for sph in c.SPHEROIDS:
     filled_sitk_image.SetSpacing(GTExpanded.GetSpacing())
     
     sitk.WriteImage(filled_sitk_image,c.PREPROCESSED_GT_DATADIR+sph+'_GT_expanded_3_DT.nrrd',useCompression=True)
-    
-    #spheroids
-    spheroid=sitk.ReadImage(c.PREPROCESSED_SPHEROIDS_DATADIR+sph+'_rs_spheroid.nrrd')
-    spheroidExpanded=sitk.Expand(spheroid,expandFactors=(1,1,h))
-    sitk.WriteImage(spheroidExpanded,c.PREPROCESSED_SPHEROIDS_DATADIR+sph+'_rs_spheroid_expanded_3.nrrd',useCompression=True)
     
     #spheroids
     spheroid=sitk.ReadImage(c.PREPROCESSED_SPHEROIDS_DATADIR+sph+'_smoothed_spheroid.nrrd')
